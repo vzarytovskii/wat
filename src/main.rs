5daf138727cde609bf1448b71374307f24aedb68
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2022-08-14 21:35:02 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate exitcode;
 
 use clap::Parser;
+use human_panic::setup_panic;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
 use std::process::ExitCode;
+use tabled::{Footer, Header, Tabled, Table, Style};
 
 #[derive(Parser, Default, Debug)]
 struct Cli {
@@ -26,6 +28,19 @@ impl<'a> FileInfo<'a> {
     }
 }
 
+#[derive(Tabled)]
+struct FileData<'a> {
+    #[tabled(rename = "File Name")]
+    file_name: &'a str,
+    #[tabled(rename = "File Path")]
+    file_path: &'a str,
+    #[tabled(rename = "File Type")]
+    file_type: &'a str,
+    #[tabled(rename = "Parsers")]
+    known_parsers: &'a str
+}
+
+
 macro_rules! exit {
     ($message:expr,$code:expr) => {
         eprintln!("{}", format!($message));
@@ -33,7 +48,7 @@ macro_rules! exit {
     };
 }
 
-fn verify_file_path(file: &PathBuf) {
+fn verify_file_path<'a>(file: &'a PathBuf) {
     let path_display = file.display();
     if !file.exists() {
         exit!("File does not exist: '{path_display}'", exitcode::NOINPUT);
@@ -45,6 +60,8 @@ fn verify_file_path(file: &PathBuf) {
 }
 
 fn main() -> ExitCode {
+    setup_panic!();
+
     let cli = Cli::parse();
     let file = cli.file_path;
 
@@ -52,7 +69,23 @@ fn main() -> ExitCode {
 
     let file_info = FileInfo::new(&file);
 
-    println!("{:?}", file_info.path);
+    let data = vec![
+        FileData {
+            file_name: file_info.path.file_name().unwrap().to_str().unwrap(),
+            file_path: file_info.path.to_str().unwrap(),
+            file_type: "Unknown",
+            known_parsers: "GenericBinaryParser, EntropyAnalyzer"
+        }
+    ];
+
+    let table =
+        Table::new(data)
+        .with(Style::rounded())
+        //.with(Header("File analysis results"))
+        //.with(Footer("Time spent: 0.00s"))
+        .to_string();
+
+    println!("{}", table);
 
     ExitCode::SUCCESS
 }
