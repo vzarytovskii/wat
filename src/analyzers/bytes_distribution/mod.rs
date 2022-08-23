707d1ea6d88428f305aa1164a930ecc364be66f5
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2022-08-23 15:10:34 +0200

    wip: Added sample analyzer

--- src/analyzers/bytes_distribution/mod.rs
+++ src/analyzers/bytes_distribution/mod.rs
@@ -1,13 +1,26 @@
-use color_eyre::Report;
-
+use crate::analyzer::Analyzer;
 use crate::FileView;
+use color_eyre::Report;
+use std::fmt::Display;
 
 pub(super) struct BytesDistributionAnalyzer<'a> {
-    pub(super) file_info: &'a FileView<'a>,
+    pub(super) file_view: &'a FileView<'a>,
 }
 
 impl<'a> BytesDistributionAnalyzer<'a> {
-    pub fn new(file_info: &'a FileView) -> Result<Self, Report> {
-        Ok(BytesDistributionAnalyzer { file_info })
+    pub fn new(file_view: &'a FileView) -> Result<Self, Report> {
+        Ok(BytesDistributionAnalyzer { file_view })
+    }
+}
+
+impl<'a> Analyzer for BytesDistributionAnalyzer<'a> {
+    fn analyze(&self, file_view: &FileView) -> Result<(), Report> {
+        Ok(())
+    }
+}
+
+impl<'a> Display for BytesDistributionAnalyzer<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "BytesDistributionAnalyzer")
     }
 }
