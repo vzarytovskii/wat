b72eedfd2df75f1025e782798e051dc0712d7a00
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2022-11-11 17:53:02 +0100

    cleanup

--- src/analyzers/mod.rs
+++ src/analyzers/mod.rs
@@ -12,7 +12,7 @@ pub(self) trait Analyzer<'a>: Display {
         Self: Sized;
 }
 
-pub(super) fn analyze(file_view: &FileView) -> Result<(), Report> {
+pub(super) fn analyze<'file_view>(file_view: &'file_view FileView) -> Result<(), Report> {
     // TODO: There must be a better way (codegen?) of instantiating the analyzers, other than manually.
     // ----: Something like going through the file and looking for the analyzer trait and instantiating it.
     // ----: Or maybe a reverse regsitration, where analyzer registers itself in the registry.
