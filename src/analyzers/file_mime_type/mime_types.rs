f29da7a767bc05e665a93c17486ea9e0853cef9d
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2023-06-07 15:13:22 +0200

    wip

--- src/analyzers/file_mime_type/mime_types.rs
+++ src/analyzers/file_mime_type/mime_types.rs
@@ -10,6 +10,7 @@ pub enum MimeType {
     Toml,
     Png,
     Elf,
+    MZ
 }
 
 #[allow(dead_code)]
@@ -25,17 +26,13 @@ pub(super) fn get_extension_mime_type(extension: &str) -> MimeType {
         .unwrap_or(MimeType::Unknown)
 }
 
-#[allow(dead_code)]
-pub(super) static MAGIC_MIME_TYPES: phf::Map<&'static str, MimeType> = phf_map! {
-    "text/x-toml" => MimeType::Toml,
-    "image/png" => MimeType::Png
-};
-
 pub(super) fn get_magic_mime_type(file_view: &FileView) -> MimeType {
     let view = file_view.view.as_ref();
+    // TODO: Probably want to move to outside of function, otherwise it will be quite big
     match view {
         [0x7f, b'E', b'L', b'F', ..] => MimeType::Elf,
         [0x89, b'P', b'N', b'G', 0x0d, 0x0a, 0x1a, 0x0a, ..] => MimeType::Png,
+        [b'M', b'Z', ..] | [b'Z', b'M', ..] => MimeType::MZ,
         _ => MimeType::Unknown,
     }
 }
