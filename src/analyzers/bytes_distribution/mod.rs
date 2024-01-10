ca4b48d5f8408c0685515faa0139b4a4503e670a
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2024-01-10 17:15:26 +0100

    Use plain canvas in byte distribution

--- src/analyzers/bytes_distribution/mod.rs
+++ src/analyzers/bytes_distribution/mod.rs
@@ -1,18 +1,14 @@
-use std::cmp;
-
 use crate::FileView;
 use async_trait::async_trait;
+use bwdraw::DuoPixel;
 use color_eyre::owo_colors::OwoColorize;
 use color_eyre::Report;
+use eyre::{eyre, ContextCompat};
 use terminal_size::{terminal_size, Height, Width};
-use textplots::{Chart, Plot, Shape};
 
 use super::{AnalysisReport, Analyzer};
 
 pub(super) struct BytesDistributionAnalyzer;
-
-const DEFAULT_WIDTH: u16 = 32;
-const DEFAULT_HEIGHT: u16 = 32;
 
 #[async_trait]
 // TODO: Positional bytes distribution analyzer (which bytes are located at which positions in the file).
@@ -23,25 +19,40 @@ impl Analyzer<'_> for BytesDistributionAnalyzer {
             .try_into()
             .expect("wrong size iterator");
         let file_len = file_view.view.len() as f32;
+
         let distribution = bytes.map(|b| {
             let count = bytecount::count(file_view.view.as_ref(), b) as f32;
-            (b as f32, count / file_len * 100.0)
+            (b, (count / file_len * 100.0) as u8) // Pretty dirty to cast to u8, but it's fine for now.
         });
 
         let (Width(w), Height(h)) =
-            terminal_size().unwrap_or((Width(DEFAULT_WIDTH), Height(DEFAULT_HEIGHT)));
-        let width = cmp::max(w, DEFAULT_WIDTH);
-        let height = cmp::max(h, DEFAULT_HEIGHT);
-        // TODO: For now, it's a simple chart, but it should be a widget that can be rendered in the tui.
-        let chart = Chart::new(width.into(), height.into(), 0.0, 255.0)
-            .lineplot(&Shape::Points(&distribution))
-            .to_string();
+            terminal_size().wrap_err_with(|| "Unsupported terminal size!".to_string())?;
+
+        if w < 256 && h < 100 {
+            return Err(eyre!("Terminal size is too small!"));
+        }
+
+        let width = 256; // includes "borders"
+        let height = 100; // includes "borders"
+
+        let mut square = bwdraw::Canvas::new(width, height);
+
+        for (x, y) in distribution {
+            let y = 100 - y; // Invert
+            let upper = y > 50;
+            let lower = y <= 50;
+            let y = y / 2;
+            //println!("{}: {}", x, y);
+            square.mut_set_duopixel(x.into(), y.into(), (upper, lower).into());
+            //.wrap_err_with(|| "Failed to set duopixel!")?;
+        }
+
         let message = format!(
-            "{}\n{  }",
-            "Bytes distribution (x = byte, y = %), automatic range for Y axis: "
+            "{}\n{}",
+            "Bytes distribution (x = byte(0-256), y = %(0-100)), automatic range for Y axis: "
                 .bold()
                 .green(),
-            chart
+            square.to_string()
         );
 
         Ok(AnalysisReport { message })
